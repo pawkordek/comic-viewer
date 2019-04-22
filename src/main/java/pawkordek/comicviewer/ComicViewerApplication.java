5d5634138e518df758360fef65558d2e6476d2e2
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2019-04-22 23:31:41 +0200

    Initialize the repository

---
+++ src/main/java/pawkordek/comicviewer/ComicViewerApplication.java
@@ -0,0 +1,12 @@
+package pawkordek.comicviewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComicViewerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComicViewerApplication.class, args);
+	}
+}
