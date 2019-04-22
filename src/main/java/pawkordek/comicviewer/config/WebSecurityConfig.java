0f53745108700b9cd0a9aa2a8131dac239924d29
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2019-04-23 00:24:37 +0200

    Fix web security being configured but not loaded

--- src/main/java/pawkordek/comicviewer/config/WebSecurityConfig.java
+++ src/main/java/pawkordek/comicviewer/config/WebSecurityConfig.java
@@ -1,9 +1,12 @@
 package pawkordek.comicviewer.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll();
