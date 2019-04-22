9df56252dae1c13659f8668020f01d0dd42e3334
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2019-04-23 00:00:55 +0200

    Add a basic security setting

---
+++ src/main/java/pawkordek/comicviewer/config/WebSecurityConfig.java
@@ -0,0 +1,11 @@
+package pawkordek.comicviewer.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll();
+    }
+}
