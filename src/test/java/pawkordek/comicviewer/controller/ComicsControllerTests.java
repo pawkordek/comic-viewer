b8c82d066e1e9484c3dda15a12ef839e23e1f06b
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2019-05-18 21:02:05 +0200

    Add tests for the index and comics controller

---
+++ src/test/java/pawkordek/comicviewer/controller/ComicsControllerTests.java
@@ -0,0 +1,29 @@
+package pawkordek.comicviewer.controller;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ComicsControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void IndexShouldMapToIndex() throws Exception {
+        mvc.perform(get("/comics"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("comics"));
+    }
+}
