7fcb9c68c931bda83c8106af90652ab6b9e0a138
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2019-05-22 19:05:54 +0200

    Add the authors table and data for it

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,13 @@
+--Comics
 insert into comics
 (title, path)
 values
 ('Kajko i Kokosz', 'kajkoko_kokosz'),
 ('Tytus, Romek i Atomek', 'tytus_romek_atomek');
+
+--Authors
+insert into authors
+(first_name, middle_name, last_name, comic_id)
+values
+('Janusz', '', 'Christa', (select id from comics where title = 'Kajko i kokosz')),
+('Henryk', 'Jerzy', 'Chmielewski', (select id from comics where title = 'Tytus, Romek i Atomek'));
--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,7 +1,20 @@
-drop table comics if exists;
+--Comics
+drop table if exists comics;
 create table comics(
     id integer not null auto_increment,
     title varchar(255) not null,
     path varchar(255) not null,
+    primary key (id)
+);
+
+--Authors
+drop table if exists authors;
+create table authors(
+    id integer not null auto_increment,
+    first_name varchar(255) not null,
+    middle_name varchar(255) default '',
+    last_name varchar(255) not null,
+    comic_id integer,
+    foreign key (comic_id) references comics(id),
     primary key (id)
 );
