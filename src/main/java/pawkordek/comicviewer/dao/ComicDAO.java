d732c405e8496e9b343cd4f0e299117cc666b2ea
Paweł Kordek <29333779+pawkordek@users.noreply.github.com>
2019-06-07 21:08:50 +0200

    Refactor Comic SQL query to be more readable

--- src/main/java/pawkordek/comicviewer/dao/ComicDAO.java
+++ src/main/java/pawkordek/comicviewer/dao/ComicDAO.java
@@ -9,6 +9,7 @@ import pawkordek.comicviewer.model.Comic;
 
 import java.util.List;
 
+
 @Repository
 public class ComicDAO {
 
@@ -17,24 +18,22 @@ public class ComicDAO {
 
     public List<Comic> getAll() {
         return jdbcTemplate.query(
-                "select c.id , c.title, c.path, " +
-                        "a.id as authors_id, a.first_name as authors_first_name, a.middle_name as authors_middle_name, a.last_name as authors_last_name, " +
-                        "r.id as authors_roles_id, r.name as authors_roles_name, " +
-                        "t.id as tags_id, t.name as tags_name " +
-                        "from comics as c " +
-                        "inner join comic_author as ca " +
-                        "on c.id = ca.comic_id " +
-                        "inner join authors as a " +
-                        "on ca.author_id = a.id " +
-                        "inner join author_author_role as ar " +
-                        "on a.id = ar.author_id " +
-                        "inner join author_roles as r " +
-                        "on ar.author_role_id = r.id " +
-                        "inner join comic_tag as ct " +
-                        "on c.id = ct.comic_id " +
-                        "inner join tags as t " +
-                        "on t.id = ct.tag_id " +
-                        "order by c.id "
+                "   SELECT " +
+                        "   c.id , c.title, c.path, " +
+                        "   a.id AS authors_id, a.first_name AS authors_first_name, " +
+                        "   a.middle_name AS authors_middle_name, a.last_name AS authors_last_name, " +
+                        "   r.id AS authors_roles_id, r.name AS authors_roles_name, " +
+                        "   t.id AS tags_id, t.name AS tags_name " +
+                        "FROM " +
+                        "   comics AS c " +
+                        "   INNER JOIN comic_author AS ca ON c.id = ca.comic_id " +
+                        "   INNER JOIN authors AS a ON ca.author_id = a.id " +
+                        "   INNER JOIN author_author_role AS ar ON a.id = ar.author_id " +
+                        "   INNER JOIN author_roles AS r ON ar.author_role_id = r.id " +
+                        "   INNER JOIN comic_tag AS ct ON c.id = ct.comic_id " +
+                        "   INNER JOIN tags AS t ON t.id = ct.tag_id " +
+                        "ORDER BY " +
+                        "   c.id "
                 , comicsExtractor);
     }
 
